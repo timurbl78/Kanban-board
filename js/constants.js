5bac5533415f3078091abc77f911d95604a7afa9
Тимур Белов <timurbl574@gmail.com>
2021-01-11 12:47:18 +0300

    add data, consts, utils js files

---
+++ js/constants.js
@@ -0,0 +1,18 @@
+export const Status = {
+  BACKLOG: `backlog`,
+  PROCESSING: `processing`,
+  DONE: `done`,
+  BASKET: `basket`,
+};
+
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`,
+}
+
+export const InsertPosition = {
+  BEFOREBEGIN: `beforebegin`,
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`,
+  AFTEREND: `afterend`,
+}
---
+++ js/data.js
@@ -0,0 +1,42 @@
+export const tasks = [
+  {
+    'id': `1`,
+    'title': `Выучить JS`,
+    'status': `backlog`,
+  },
+  {
+    'id': `2`,
+    'title': `Выучить React`,
+    'status': `backlog`,
+  },
+  {
+    'id': `3`,
+    'title': `Покушать`,
+    'status': `processing`,
+  },
+  {
+    'id': `4`,
+    'title': `Отдохнуть`,
+    'status': `processing`,
+  },
+  {
+    'id': `5`,
+    'title': `Сходить погулять`,
+    'status': `done`,
+  },
+  {
+    'id': `6`,
+    'title': `Перестать тупить`,
+    'status': `processing`,
+  },
+  {
+    'id': `7`,
+    'title': `Сходить в спорткомплекс`,
+    'status': `backlog`,
+  },
+  {
+    'id': `8`,
+    'title': `Прочитать книгу`,
+    'status': `basket`,
+  }
+];
